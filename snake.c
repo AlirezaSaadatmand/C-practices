b2071adfc083ef1daa72bc97cc8c45b8b3cce59d
Alireza <alirezasaadat056@gmail.com>
2024-10-11 11:57:27 +0330

    snake movement

--- snake.c
+++ snake.c
@@ -13,7 +13,7 @@ int x = WIDTH / 2 , y = HEIGHT / 2;
 
 int fruitX , fruitY;
 
-int snakeLength = 5;
+int snakeLength = 1;
 
 int moveKey = 0;
 
@@ -22,24 +22,18 @@ bool gameover = false;
 void createFood(){
     fruitX = rand() % WIDTH;
     fruitY = rand() % HEIGHT;
-    for (int i = 0;i < snakeLength;i++){
-        if (fruitX == snakeX[i] && fruitY == snakeY[i]){
-            createFood();
-            break;
-        }
-    }
 }
 
 void userInput(){
     if (kbhit()){
         char move = tolower(getch());
-        if (move == 'w'){
+        if (move == 'w' && moveKey != 3){
             moveKey = 1;
-        }else if(move == 'a'){
+        }else if(move == 'a' && moveKey != 4){
             moveKey = 2;
-        }else if(move == 's'){
+        }else if(move == 's' && moveKey != 1){
             moveKey = 3;
-        }else if(move == 'd'){
+        }else if(move == 'd' && moveKey != 2){
             moveKey = 4;
         }
     }
@@ -53,6 +47,8 @@ void draw(){
                 printf("#");
             }else if (j == 0 || j == WIDTH - 1){
                 printf("#");
+            }else if (j == fruitX && i == fruitY){
+                printf("X");
             }else{
                 bool isSnake = false;
                 for (int k = 0;k < snakeLength ;k++){
@@ -64,6 +60,7 @@ void draw(){
                 if (!isSnake){
                     printf(" ");
                 }
+
             }
         }
         printf("\n");
@@ -94,6 +91,15 @@ void movement(){
     }else if(moveKey == 4){
         x++;
     }
+
+    if (x == fruitX && y == fruitY){
+        snakeLength++;
+        createFood();
+    }
+
+    if (x < 0 || y < 0 || x > WIDTH || y > HEIGHT){
+        gameover = true;
+    }
 }
 
 int main(){
@@ -103,8 +109,8 @@ int main(){
 
 
     while (!gameover){
-        userInput();
         movement();
+        userInput();
         draw();
         Sleep(33);
     }
