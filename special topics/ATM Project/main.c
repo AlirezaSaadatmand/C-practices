b429abff18576187b86b52b1c0a8f4390652fed3
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2024-12-25 21:35:29 +0330

    test

--- special topics/ATM Project/main.c
+++ special topics/ATM Project/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+int prime(int number);
+
 int main(){
-	printf("hello world!");
+	int a , b;
+	scanf("%d %d" , &a , &b);
+
+	for (int i = a ; i < b ; i++){
+		if (prime(i)){
+			printf("%d \t" , i);
+		}
+	}
 	return 0;
+}
+
+int prime(int number){
+	int count = 0;
+	for (int i = 2 ; i < number ; i++){
+		if (number % i == 0){
+			count++;
+		}
+	}
+	if (count == 0){
+		return 1;
+	}else{
+		return 0;
+	}
 }
