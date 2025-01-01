f97842eafa091cfe80caae6448795d761374e9d3
AlirezaSaadatmand <alirezasaadat056@gmail.com>
2025-01-01 20:32:21 +0330

    homework 1

--- special topics/homework 5 (structure)/homework1.c
+++ special topics/homework 5 (structure)/homework1.c
@@ -10,10 +10,15 @@ struct Date {
 int main(){
     int n;
     scanf("%d", &n);
+    struct Date dates[n];
     struct Date date;
     for (int i = 0 ; i < n ; i++){
         scanf("%d %d %d" , &date.day , &date.month , &date.year);
-        printf("%d.%d.%d", date.year , date.month , date.day);
+        dates[i] = date;
+    }
+
+    for (int date = 0 ; date < n ; date++){
+        printf("%d.%d.%d\n", dates[date].year , dates[date].month , dates[date].day);
     }
     return 0;
 }
